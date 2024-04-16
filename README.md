55982f406030c382e3a059974e53875d6166994c
Stefano Mondino <stefano.mondino.dev@gmail.com>
2024-04-16 22:20:08 +0200

    Instructions in readme  + some test tweak

--- README.md
+++ README.md
@@ -3,3 +3,40 @@
 This is the basic project setup for SwiftHeroes 2024 workshop
 
 We'll add more data to this as we proceed with the workshop :)
+
+## Setting up the environment
+
+### Mise
+
+To install mise, you can run this three commands:
+
+```bash
+curl https://mise.jdx.dev/install.sh | sh
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+```
+
+and then restart the terminal.
+
+### Installing Tuist
+
+From this project folder, just run 
+
+```bash
+mise install
+```
+
+and wait for it <3
+
+### Tuist useful commands
+
+```
+# Installing project dependencies
+tuist install 
+
+# Generating the project
+tuist generate
+
+# Create and open the project graph
+tuist graph
+```
--- Workshop/Tests/WorkshopTests.swift
+++ Workshop/Tests/WorkshopTests.swift
@@ -9,30 +9,46 @@ final class WorkshopTests: XCTestCase {
     
     struct TestRepository: TalkRepository {
         func talks() async throws -> [Workshop.Talk] {
-            [.mock]
+            [.mockWithSpeaker, .mockWithoutSpeaker]
         }
     }
     
-    func test_navigate_to_detail() throws {
+    func test_navigate_to_talk_detail() throws {
         let router = Router(container: .init())
         let viewModel = TalksScene.ViewModel(router: router,
                                              repository: TestRepository())
         expectRouteDefinition(on: viewModel,
-                                  equalTo: TalkDetail.RouteDefinition(talk: .mock)) { viewModel in
-            viewModel.open(viewModel: .init(talk: .mock))
-        } 
+                                  equalTo: TalkDetail.RouteDefinition(talk: .mockWithSpeaker)) { viewModel in
+            viewModel.open(viewModel: .init(talk: .mockWithSpeaker))
+        }
     }
 }
 
 extension Talk {
-    static var mock: Talk {
+    static var mockWithSpeaker: Talk {
         .init(id: 1,
               code: "MOCK",
               title: "Test", abstract: "Test abstract",
               duration: 10,
+              speakers: [.mock],
+              room: .init(id: 1, name: "Name"),
+              start: .init(timeIntervalSince1970: 100_000_000),
+              end: .init(timeIntervalSince1970: 100_003_600))
+    }
+    static var mockWithoutSpeaker: Talk {
+        .init(id: 1,
+              code: "LUNCH",
+              title: "Lunch", abstract: "Lunch time",
+              duration: 10,
               speakers: [],
-              room: nil,
-              start: nil,
-              end: nil)
+              room: .init(id: 1, name: "Name"),
+              start: .init(timeIntervalSince1970: 100_010_000),
+              end: .init(timeIntervalSince1970: 100_013_600))
+    }
+}
+
+extension Speaker {
+    static var mock: Speaker {
+        .init(code: "MOCK", name: "Mock n roll", avatar: nil)
     }
 }
